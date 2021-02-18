f02ad76b3d0f7b9e029c7ff6582ee61431008948
Lufei Shuang <shuanglufei1994@gmail.com>
2021-02-19 00:35:44 +0700

    use cors

--- app.ts
+++ app.ts
@@ -7,6 +7,7 @@ export const appName = 'wulei';
 
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 import setUpApiv1 from './routes/apiv1/setUpApiv1';
 import { apiv1 } from './routes/apiv1/init-routes';
 
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 app.use('/api-v1', apiv1);
 
 app.listen(3000, () => {
