b1b8dc7274951202811bbb13326550c4cb764a44
Lufei Shuang <shuanglufei1994@gmail.com>
2021-04-06 15:52:23 +0700

    cover letter redirection

--- src/routes/author-profile/index.ts
+++ src/routes/author-profile/index.ts
@@ -4,9 +4,13 @@ import { authorProfile } from "./init-author-profile";
 
 const apiPrefix = "/borischen_profile";
 
-const profileUrl = process.env.BC_PROFILE || "";
+const profileUrl = process.env.WE_BC_PROFILE || "";
+const coverLetterUrl = process.env.WE_BC_COVER_LETTER || "";
 
 authorProfile.get("", logIp, initLogPathHandler(apiPrefix), (req, res) => {
+    if (req.query.from === 'indeed.com') {
+        res.redirect(coverLetterUrl);
+    }
     res.redirect(profileUrl);
 });
 
